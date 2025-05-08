42740ec03838ee72149f3ef1839831d69386a1d9
Muhammad Hassan <hassanaslamkellar@gmail.com>
2025-05-08 14:51:25 +0500

    Add files via upload

---
+++ protfolio/script.js
@@ -0,0 +1,8 @@
+ var menu = document.querySelector("menu");
+ var nav = document.querySelector("links");
+
+ menu.oneclick=() =>{
+    
+     menu.classList. toggle("bx-x");
+     nav.classList. toggle("active");
+}
